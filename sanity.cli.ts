2fec9504e500098003fd0b14af3aa5719bd160f6
Marcelo Augusto <nextlevelcode014@gmail.com>
2025-03-04 07:07:16 -0300

    Updated schema for production

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'uzhojt7x',
-    dataset: 'production'
+    dataset: 'development'
   },
   /**
    * Enable auto-updates for studios.
